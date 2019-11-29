fcdf4592f0c3bb850fbeaa26b0a32db625b86922
Max <maxim00066@gmail.com>
2019-11-29 16:50:38 +0300

    fixed page-footer accordion

--- src/blocks/page-footer/page-footer.js
+++ src/blocks/page-footer/page-footer.js
@@ -3,6 +3,7 @@ const ready = require('../../js/utils/documentReady.js');
 
 ready(function () {
   const footerAccordion = () => {
+    const itemTitle = document.querySelectorAll('.page-footer__item-title');
     const items = document.querySelectorAll('.page-footer__item');
     const lists = document.querySelectorAll('.page-footer__sublist');
     const icons = document.querySelectorAll('.page-footer__item-icon');
@@ -12,8 +13,9 @@ ready(function () {
     }
 
     if (getWindowWidth() < 1200) {
-      for (let i = 0; i < items.length; i++) {
-        items[i].addEventListener('click', () => {
+      for (let i = 0; i < itemTitle.length; i++) {
+        itemTitle[i].addEventListener('click', () => {
+          items[i].classList.toggle('page-footer__item--active');
           lists[i].classList.toggle('page-footer__sublist--active');
           icons[i].classList.toggle('page-footer__item-icon--active');
         })
